48147bd7ca5491f0afc699a8c31cb55bc6857a70
sarvani-harshita <harshi.sarvani@gmail.com>
2021-11-11 23:18:49 +0530

    added simple directive

--- src/app/directive/simpledirective.directive.ts
+++ src/app/directive/simpledirective.directive.ts
@@ -1,10 +1,28 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Input, Renderer2 } from '@angular/core';
 
 @Directive({
-  selector: '[appSimpledirective]'
+  selector: 'appSimpledirective,[appSimpledirective] '
 })
 export class SimpledirectiveDirective {
+  @Input('appSimpledirective') somecolor: string = 'pink'
+@HostBinding('style.backgroundColor') backgroundColor: string 
+  constructor(private element: ElementRef, private renderer: Renderer2) { }
+  ngOnInit(){
+    this.backgroundColor = this.somecolor;
+    // (<HTMLElement>this.element.nativeElement).style.color = 'pink';
+    // this.renderer.setStyle(this.element.nativeElement, 'background-color', 'grey')
+    // console.log(this.element, this.renderer);
+  }
+  
+  // @HostListener('mouseenter') onmouseover(event :Event) {
+  //   this.renderer.setStyle(this.element.nativeElement, 'background-color', 'blue')
+  // }
+  // @HostListener('mouseleave') onmouseexit(event :Event) {
+  //   this.renderer.setStyle(this.element.nativeElement, 'background-color', 'grey')
+  // }
 
-  constructor() { }
+  @HostListener('mouseover') onmouseexit(event :Event) {
+    this.renderer.setStyle(this.element.nativeElement, 'background-color', 'BlueViolet')
+  }
 
 }
